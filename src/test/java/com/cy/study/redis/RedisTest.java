2c930d1c230db63d1b8c9df73b39c518eff06d5a
caoyang <aries_caoyang@163.com>
2020-05-29 14:34:43 +0800

    redis工具类

---
+++ src/test/java/com/cy/study/redis/RedisTest.java
@@ -0,0 +1,32 @@
+package com.cy.study.redis;
+
+import com.cy.study.elasticsearch.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.*;
+
+/**
+ * redis测试类
+ *
+ * @author cy
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void stringTest(){
+        Set<Object> vv = RedisUtils.sGet("vv");
+        System.out.println(vv.size());
+    }
+
+}
