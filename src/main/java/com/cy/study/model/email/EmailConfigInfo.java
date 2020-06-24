3b6f4dc3544aa10c46c3aaeec456a763537c6d69
caoyang <aries_caoyang@163.com>
2020-06-24 10:33:36 +0800

    代码优化

--- src/main/java/com/cy/study/model/email/EmailConfigInfo.java
+++ src/main/java/com/cy/study/model/email/EmailConfigInfo.java
@@ -1,5 +1,6 @@
 package com.cy.study.model.email;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
  *
  * @author cy
  */
+@Data
 @Component
 @ConfigurationProperties(prefix = "email")
 public class EmailConfigInfo {
@@ -15,19 +17,4 @@ public class EmailConfigInfo {
     private String username;
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
