e541e7cc12694ef39c13c6b4fde2bea315bf913b
caoyang <aries_caoyang@163.com>
2020-08-03 14:01:36 +0800

    curator框架zookeeper分布式demo

--- src/main/java/com/cy/study/zk/ZkTest.java
+++ src/main/java/com/cy/study/zk/ZkTest.java
@@ -1,13 +1,10 @@
 package com.cy.study.zk;
 
-import org.I0Itec.zkclient.IZkDataListener;
-import org.I0Itec.zkclient.ZkClient;
-import org.I0Itec.zkclient.exception.ZkNodeExistsException;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 
 /**
  * 测试
@@ -22,110 +19,41 @@ import java.util.concurrent.locks.Lock;
  *  9.加锁成功
  * @author cy
  */
-public class ZkTest  implements Runnable, Lock {
+public class ZkTest  {
 
-    static int inventory = 10;
-    private static final int NUM = 10;
-    private static CountDownLatch cdl = new CountDownLatch(1);
+    public static void main(String[] args) throws Exception {
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(
+                "127.0.0.1",
+                5000,
+                4000,
+                new ExponentialBackoffRetry(4000, 3)
+        );
+        //建立连接
+        curatorFramework.start();
 
-    private static final String IP_PORT = "127.0.0.1:2181";
-    private static final String Z_NODE = "/LOCK";
+        //创建节点目录
+        InterProcessMutex lock = new InterProcessMutex(curatorFramework, "/LOCKS");
 
-    private static ZkClient zkClient = new ZkClient(IP_PORT);
-
-
-    public static void main(String[] args) {
-        try {
-
-            for (int i = 1; i <= NUM; i++) {
-                new Thread(new ZkTest()).start();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        for(int i=0;i<10;i++){
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+"尝试获取锁");
+                try {
+                    //加锁
+                    lock.acquire();
+                    System.out.println(Thread.currentThread().getName()+"获取锁成功");
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        lock.release();
+                        System.out.println("释放锁");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
-    }
 
-    @Override
-    public void run() {
-        try {
-            new ZkTest().lock();
-            if (inventory > 0) {
-                inventory--;
-            }
-            System.out.println(inventory);
-            return;
-        } finally {
-            new ZkTest().unlock();
-            System.out.println("释放锁");
-        }
-    }
-
-    @Override
-    public void lock() {
-        // 尝试加锁
-        if(tryLock()){
-            return;
-        }
-        // 进入等待 监听
-        waitForLock();
-        // 再次尝试
-        lock();
-    }
-
-    @Override
-    public boolean tryLock() {
-        try {
-            zkClient.createPersistent(Z_NODE);
-            return true;
-        }catch (ZkNodeExistsException e){
-            return false;
-        }
-    }
-
-    @Override
-    public void unlock() {
-        zkClient.delete(Z_NODE);
-    }
-
-    public void waitForLock(){
-        System.out.println("加锁失败");
-        IZkDataListener listener = new IZkDataListener() {
-            @Override
-            public void handleDataChange(String s, Object o) throws Exception {
-
-            }
-
-            @Override
-            public void handleDataDeleted(String s) throws Exception {
-                System.out.println("唤醒");
-                cdl.countDown();
-            }
-        };
-        // 监听
-        zkClient.subscribeDataChanges(Z_NODE, listener);
-        if (zkClient.exists(Z_NODE)) {
-            try {
-                cdl.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        // 释放监听
-        zkClient.unsubscribeDataChanges(Z_NODE, listener);
-    }
-
-    @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
-    }
-
-    @Override
-    public void lockInterruptibly() throws InterruptedException {
-
-    }
-
-    @Override
-    public Condition newCondition() {
-        return null;
     }
 }
