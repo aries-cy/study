abcf6f49b45d60a6c186c5baa09e7e4de8c74f0c
caoyang <aries_caoyang@163.com>
2020-09-10 16:26:14 +0800

    spring中的多个service实现--map

--- src/main/java/com/cy/study/beautify/BeautifyTest.java
+++ src/main/java/com/cy/study/beautify/BeautifyTest.java
@@ -4,6 +4,8 @@ import com.cy.study.beautify.config.MyConfiguration;
 import com.cy.study.beautify.service.PayService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+
 /**
  * 测试
  *
@@ -13,6 +15,9 @@ public class BeautifyTest {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfiguration.class);
+
+        Map<String, PayService> map = context.getBeansOfType(PayService.class);
+
         PayService weChatService = (PayService) context.getBean("WeChat");
         PayService alipayService = (PayService) context.getBean("Alipay");
         weChatService.execute();
